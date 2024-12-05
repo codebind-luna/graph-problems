d5e2f4afc8583a362d1a2523f463ff993b94150c
Luna Das <cse2013luna@gmail.com>
2024-12-05 20:01:14 +0530

    Add solution for LC-3286

--- find.a.safe.walk.through.a.grid/main.go
+++ find.a.safe.walk.through.a.grid/main.go
@@ -1,46 +1,75 @@
+/*
+	Leetcode problem: https://leetcode.com/problems/find-a-safe-walk-through-a-grid/
+*/
 package main
+
+import (
+	"container/heap"
+	"math"
+)
 
 type pair struct {
 	r, c, h int
 }
 
+type minHeap []pair
+
+func (mh minHeap) Len() int {
+	return len(mh)
+}
+
+func (mh minHeap) Less(i int, j int) bool {
+	return mh[i].h > mh[j].h
+}
+
+func (mh minHeap) Swap(i int, j int) {
+	mh[i], mh[j] = mh[j], mh[i]
+}
+
+func (mh *minHeap) Push(a interface{}) {
+	*mh = append(*mh, a.(pair))
+}
+
+func (mh *minHeap) Pop() interface{} {
+	l := len(*mh)
+	res := (*mh)[l-1]
+	*mh = (*mh)[:l-1]
+	return res
+}
+
 func findSafeWalk(grid [][]int, health int) bool {
-	rows, cols := len(grid), len(grid[0])
 	directions := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-	visited := make([][]bool, rows)
+	rows, cols := len(grid), len(grid[0])
+	healthCnt := make([][]int, rows)
 	for i := 0; i < rows; i++ {
-		visited[i] = make([]bool, cols)
+		healthCnt[i] = make([]int, cols)
+		for j := 0; j < cols; j++ {
+			healthCnt[i][j] = math.MinInt
+		}
 	}
 
-	var q []pair
+	var mh minHeap
+	heap.Init(&mh)
+	heap.Push(&mh, pair{r: 0, c: 0, h: health - grid[0][0]})
+	healthCnt[0][0] = 0
 
-	p := pair{r: 0, c: 0, h: health}
-	if grid[0][0] == 1 {
-		p.h -= 1
-	}
-	q = append(q, p)
-	visited[0][0] = true
-
-	for len(q) > 0 {
-		e := q[0]
-		q = q[1:]
-
+	for mh.Len() > 0 {
+		e := heap.Pop(&mh).(pair)
 		r, c, h := e.r, e.c, e.h
 
-		if r == rows-1 && c == cols-1 {
-			return h > 0
-		}
+		for _, d := range directions {
+			nr, nc := r+d[0], c+d[1]
 
-		if h > 0 {
-			for _, d := range directions {
-				nr, nc := r+d[0], c+d[1]
-				if (0 <= nr && nr < rows && 0 <= nc && nc < cols) && !visited[nr][nc] {
-					nh := h - grid[nr][nc]
-					visited[nr][nc] = true
-					q = append(q, pair{r: nr, c: nc, h: nh})
+			if 0 <= nr && nr < rows && 0 <= nc && nc < cols {
+				nh := h - grid[nr][nc]
+
+				if nh > healthCnt[nr][nc] {
+					healthCnt[nr][nc] = nh
+					heap.Push(&mh, pair{r: nr, c: nc, h: nh})
 				}
 			}
 		}
 	}
-	return false
+
+	return healthCnt[rows-1][cols-1] >= 1
 }
--- shortest.path.in.grid.after.obstacle.removal/main.go
+++ shortest.path.in.grid.after.obstacle.removal/main.go
@@ -1,34 +1,30 @@
 package main
 
 type pair struct {
-	r     int
-	c     int
-	dis   int
-	eLeft int
+	r int
+	c int
+	d int
+	e int
 }
 
 type state struct {
-	r     int
-	c     int
-	eLeft int
+	r int
+	c int
+	e int
 }
 
 func shortestPath(grid [][]int, k int) int {
 	rows, cols := len(grid), len(grid[0])
 
-	if k >= rows+cols-2 {
-		return rows + cols - 2
-	}
-
 	var q []pair
-	q = append(q, pair{r: 0, c: 0, dis: 0, eLeft: k})
+	q = append(q, pair{r: 0, c: 0, d: 0, e: 0})
 	seen := make(map[state]bool)
-	seen[state{r: 0, c: 0, eLeft: k}] = true
+	seen[state{r: 0, c: 0, e: 0}] = true
 
 	for len(q) > 0 {
 		item := q[0]
 		q = q[1:]
-		row, col, dis, eLeft := item.r, item.c, item.dis, item.eLeft
+		row, col, dis, e := item.r, item.c, item.d, item.e
 
 		if row == rows-1 && col == cols-1 {
 			return dis
@@ -38,12 +34,12 @@ func shortestPath(grid [][]int, k int) int {
 			newRow, newCol := row+direction[0], col+direction[1]
 
 			if newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols {
-				newEliminations := eLeft - grid[newRow][newCol]
+				newEliminations := e + grid[newRow][newCol]
 
-				if newEliminations >= 0 {
-					if !seen[state{r: newRow, c: newCol, eLeft: newEliminations}] {
-						seen[state{r: newRow, c: newCol, eLeft: newEliminations}] = true
-						q = append(q, pair{r: newRow, c: newCol, dis: dis + 1, eLeft: newEliminations})
+				if newEliminations <= k {
+					if !seen[state{r: newRow, c: newCol, e: newEliminations}] {
+						seen[state{r: newRow, c: newCol, e: newEliminations}] = true
+						q = append(q, pair{r: newRow, c: newCol, d: dis + 1, e: newEliminations})
 					}
 				}
 			}
